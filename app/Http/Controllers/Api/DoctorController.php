05a68c9b6a61aada61ec9632854218c582b21e70
fatma166 <fatmaghareeb90@gmail.com>
2022-11-01 15:42:26 +0200

    1-11

--- app/Http/Controllers/Api/DoctorController.php
+++ app/Http/Controllers/Api/DoctorController.php
@@ -9,16 +9,30 @@ use App\Http\Resources\DectorDetailsResource;
 use App\Http\Resources\ServiceTypeFeeResource ;
 class DoctorController extends Controller
 {
+    /****
+     * 
+     * 
+     * types     /all/   or /online only/  or /count online doctor     
+     * 
+     */
      
-    public function list(Request $request){
+    public function list(Request $request,$type='all'){   
        
         $start=0;
         $end=2;
         $doctor= new DoctorRepositories;
-        $doctors= $doctor->list($request);
-
-        $data=array(DectorDetailsResource::collection($doctors));
-       return($data);
+        $doctors= $doctor->list($request,$type);
+        if ($type=='countonline')
+            $data=array('count'=>$doctors);
+        else
+            $data=array(DectorDetailsResource::collection($doctors));
+      // return($data);
       //  print_r($doctors);
+        return response()->json([
+            'status' => 'success',
+            'data' =>$data,
+
+            ],200);
+  
     }
 }
